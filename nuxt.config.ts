dc61c7dbd833492c2c76b2240363dcaa0bbddc1d
Alex <alex26g4@gmail.com>
2023-05-21 10:34:52 +0300

    tailwind init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content','nuxt-icon','@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/assets/css/main.css',
+  },
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+    highlight: {
+      theme: {
+        // Default theme (same as single string)
+        default: 'material-palenight',
+        // Theme used if `html.dark`
+        dark: 'github-dark',
+      }
+    }
+  },
 })
