a27dcaec71292134a7e86463407708c29341d2cd
Alex <alex26g4@gmail.com>
2023-05-22 07:30:50 +0300

    creation of default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex justify-between p-4 bg-green-500 text-white">
+        <NuxtLink to="/"><img class="w-8" src="/logo/logo.svg" alt="SVG Image"></NuxtLink>
+        <ul class="flex gap-4">
+            <li>
+                <NuxtLink to="/events">Events</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/events">Events</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/events">Events</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
