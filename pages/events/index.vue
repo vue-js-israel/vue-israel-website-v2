8069dc0db996b438d3f789bfbd5ac612e5070dbe
Alex <nechaev26g4@gmail.com>
2023-05-23 16:14:40 +0300

    improvment of event index page

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,26 +1,55 @@
-<template>
-    <NuxtLayout/>
-    <div class="max-w-7xl m-auto">
-        <EventsEventCard :events="events"></EventsEventCard>
-    </div>
-</template>
-
 <script setup>
-
-
-
-const contentQuery = await queryContent('events').find()
-const events = contentQuery.map((content) => {
-    const { _path, title, location, date } = content;
-    console.log("👾 ~ file: index.vue:14 ~ events ~ content:", content)
-    return {
-        fileName: _path.split('/')[2],
-        title,
-        location,
-        date
-    }
-})
-
+useHead({
+  title: "All articles",
+  meta: [
+    { name: "description", content: "Here's a list of all my great articles" },
+  ],
+});
 </script>
 
-<style scoped></style>
+<template>
+  <NuxtLayout />
+  <main>
+    <header class="page-heading">
+      <div class="wrapper">
+        <h1 class="text-5xl font-extrabold">Events</h1>
+        <p class="text-lg font-medium">event of Vus.JS Israel</p>
+      </div>
+    </header>
+    <section class="page-section">
+      <Tags />
+      <!-- Render list of all articles in ./content/blog using `path` -->
+      <!-- Provide only defined fields in the `:query` prop -->
+      <ContentList
+        path="/events"
+        :query="{
+          only: ['title', 'description', 'date', 'location', 'tags', '_path'],
+        }"
+      >
+        <!-- Default list slot -->
+        <template v-slot="{ list }">
+          <ul class="event-list">
+            <li v-for="event in list" :key="event._path" class="event">
+              <EventsEventCard :event="event" />
+            </li>
+          </ul>
+        </template>
+
+        <!-- slot to display message when no content is found -->
+        <template #not-found>
+          <p>No articles found.</p>
+        </template>
+      </ContentList>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+
+.page-section{
+  @apply max-w-xl m-auto;
+}
+.event-list {
+  @apply m-auto my-3 flex max-w-md flex-col gap-3;
+}
+</style>
