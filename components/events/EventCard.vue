d9061cdfe69d897913ea4e27fefc12a9fff41c79
Alex <nechaev26g4@gmail.com>
2023-05-23 16:18:11 +0300

    tags added to event card

--- components/events/EventCard.vue
+++ components/events/EventCard.vue
@@ -1,41 +1,54 @@
 <template>
-    <NuxtLink v-for="event in events" :to="`/events/${event.fileName}`">
-        <div class="bg-white rounded-lg shadow-md p-6 m-auto my-3 max-w-md">
-            <h3 class="text-sm font-medium text-gray-500">{{ formattedDate(event.date) }}</h3>
-            <h2 class="text-xl font-bold mb-2 text-gray-600">{{ event.title }}</h2>
-            <div class="flex items-center">
-                <Icon name="mdi:map-marker-outline" />
-                <p>{{ event.location }}</p>
-            </div>
-        </div>
-    </NuxtLink>
+  <NuxtLink :to="`${event._path}`">
+    <div class="rounded-lg bg-white p-6 shadow-md">
+      <h3 class="text-sm font-medium text-gray-500">
+        {{ formattedDate(event.date) }}
+      </h3>
+      <h2 class="mb-2 text-xl font-bold text-gray-600">{{ event.title }}</h2>
+      <div class="flex items-center">
+        <Icon name="mdi:map-marker-outline" />
+        <p>{{ event.location }}</p>
+      </div>
+      <ul class="tag-container">
+        <li v-for="tag in event.tags" class="tag">{{ tag }}</li>
+      </ul>
+    </div>
+  </NuxtLink>
 </template>
 
 <script setup>
 const props = defineProps({
-    events: {
-        type: Array,
-        require: true,
-    }
-})
+  event: {
+    type: Object,
+    require: true,
+  },
+});
 
 const formattedDate = (date) => {
-    const formattedDate = new Date(date);
+  const formattedDate = new Date(date);
 
-    const options = {
-        weekday: "short",
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-        hour: "numeric",
-        hour12: true,
-        minute: "numeric",
-        timeZoneName: "shortGeneric",
-        timeZone: "Asia/Jerusalem"
-    };
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    hour12: true,
+    minute: "numeric",
+    timeZoneName: "shortGeneric",
+    timeZone: "Asia/Jerusalem",
+  };
 
-    return formattedDate.toLocaleString("en-IL", options).toUpperCase();
-}
+  return formattedDate.toLocaleString("en-IL", options).toUpperCase();
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.tag-container {
+  @apply flex gap-1 my-2;
+}
+
+.tag-container .tag {
+  @apply rounded-md bg-gray-200 p-0.5 text-xs;
+}
+</style>
