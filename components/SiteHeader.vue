e51d55639f2a89fe67622f53efa4e950bec7465f
Dima Vishnevetsky <dimshik100@gmail.com>
2023-05-24 22:39:34 +0300

    Added hnew header and footer + Updated layout

---
+++ components/SiteHeader.vue
@@ -0,0 +1,33 @@
+<!-- ./components/SiteHeader.vue -->
+
+<template>
+    <header class="site-header">
+      <div class="wrapper">
+        <NuxtLink to="/" class="no-underline flex">
+            <img class="w-8 mr-2" src="/logo/logo.svg" alt="Vue.js Israel Community Logo"> Vue.js Israel Community
+        </NuxtLink>
+
+        <nav class="site-nav">
+          <ul class="links">
+            <li class="link">
+                <NuxtLink to="/events">Events</NuxtLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </template>
+  
+  <style scoped>
+  .site-header {
+    @apply sticky top-0 w-full p-4 bg-emerald-100 bg-opacity-40 border-b-2 border-white border-opacity-30 backdrop-blur-lg z-20;
+  }
+  
+  .site-header > .wrapper {
+    @apply flex items-center justify-between max-w-6xl m-auto;
+  }
+  
+  .site-logo {
+    @apply font-black text-lg;
+  }
+  </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,5 @@
 <template>
-    <div class="flex justify-between p-4 bg-green-500 text-white">
-        <NuxtLink to="/"><img class="w-8" src="/logo/logo.svg" alt="SVG Image"></NuxtLink>
-        <ul class="flex gap-4">
-            <li>
-                <NuxtLink to="/events">Events</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/events">Events</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="/events">Events</NuxtLink>
-            </li>
-        </ul>
-    </div>
+    <SiteHeader />
+    <slot></slot>
+    <SiteFooter />
 </template>
