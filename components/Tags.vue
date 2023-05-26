f827646a54db050c58c988ed0fc1a3a3b38c2754
Alex <alex26g4@gmail.com>
2023-05-26 21:18:45 +0300

    improvement of Tags functionality

--- components/Tags.vue
+++ components/Tags.vue
@@ -1,48 +1,38 @@
 <script setup>
-// import icon
-// import { TagIcon } from "@heroicons/vue/solid";
+
+const props = defineProps({
+  tags: {
+    type: Array,
+    required: true,
+  }
+})
+
+const emit = defineEmits(['tagClick'])
 
 // tag list state
 const expanded = ref(false);
-
-// helper function to flatten tags array
-const flatten = (tags, key) => {
-  let _tags = tags
-    .map((tag) => {
-      let _tag = tag;
-      if (tag[key]) {
-        let flattened = flatten(tag[key]);
-        _tag = flattened;
-      }
-      return _tag;
-    })
-    .flat(1);
-  return _tags;
-};
 
 // function to toggle expanded state
 const toggleExpand = () => {
   expanded.value = !expanded.value;
 };
 
-// get only tags data from `/events`
-const { data } = await useAsyncData("tags", () => queryContent("events").only(["tags"]).find());
-
-// generate array without duplicates from flattened array
-const eventTags = [...new Set(flatten(data.value, "tags"))];
+const onTagClickHandler = (tagIndex) => {
+  emit('tagClick',tagIndex);
+}
 </script>
 <template>
   <div class="tag-list">
     <!-- Button to toggle expand -->
     <button @click="toggleExpand" class="cta w-icon">
-        <Icon name="mdi:tag-outline" />
-        <span>Tags</span>
+      <Icon name="mdi:tag-outline" />
+      <span>Tags</span>
     </button>
     <ul class="event-tags" :class="{ expanded: expanded }">
       <!-- list out tags with links -->
-      <li v-for="(tag, n) in eventTags" :key="n" class="tag">
-        {{ tag }}
-        <NuxtLink :to="`/events/tags/${tag}`" class="font-semibold"/>
+      <li v-for="(tag, index) in tags" :key="`${tag}-${index}`" @click="onTagClickHandler(index)" class="tag"
+        :class="{ selected: tag.selected }">
+        <a>{{ tag.title }}</a>
       </li>
     </ul>
   </div>
@@ -53,9 +43,20 @@ const eventTags = [...new Set(flatten(data.value, "tags"))];
   @apply flex items-center gap-2 p-2 rounded-lg;
 }
 .event-tags {
-    @apply grid grid-cols-4 transition-all max-w-0 overflow-hidden;
+  @apply py-1;
+  @apply grid grid-cols-4 transition-all max-w-0 overflow-hidden gap-2;
+  @apply select-none;
   /* @apply transition-all max-w-0 max-h-10 overflow-hidden flex flex-row justify-center content-center; */
 }
+.event-tags .tag {
+  @apply rounded-md bg-slate-100 p-2 py-1 text-sm text-slate-700;
+  @apply !py-0.5 transition-all hover:-translate-y-0.5;
+  @apply text-center;
+}
+.event-tags .tag.selected {
+  @apply bg-slate-400;
+}
+
 .event-tags.expanded {
   @apply max-w-full;
 }
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,10 +1,98 @@
 <script setup>
 useHead({
-  title: "All articles",
+  title: "All Events",
   meta: [
-    { name: "description", content: "Here's a list of all my great articles" },
+    { name: "description", content: "Vus.js Israel's events" },
   ],
 });
+
+// get only tags data from `/events`
+const { data } = await useAsyncData("tags", () => queryContent("events").only(["tags"]).find());
+
+// helper function to flatten tags array
+const flatten = (tags, key) => {
+  let _tags = tags
+    .map((tag) => {
+      let _tag = tag;
+      if (tag[key]) {
+        let flattened = flatten(tag[key]);
+        _tag = flattened;
+      }
+      return _tag;
+    })
+    .flat(1);
+  return _tags;
+};
+
+
+
+const selectedTagsFromURL = () => {
+  const { fullPath } = useRoute();
+  const tagsString = (fullPath.split('='))[1];
+  return tagsString?.split(',') ?? [];
+}
+
+const selectedTags = ref(selectedTagsFromURL());
+
+/**
+ * generates an array without duplicates from flattened array
+ * and initiate each instance with the title and selected property.
+ */
+const interactiveTags = () => {
+  const tags = [...new Set(flatten(data.value, "tags"))];
+  const interactiveTags = tags.map((tag) => {
+
+    const selected = selectedTags.value.includes(tag);
+
+    return {
+      title: tag,
+      selected,
+    }
+  })
+  return interactiveTags;
+};
+const eventTags = ref(interactiveTags())
+
+const updateURLParams = () => {
+  const key = "tags";
+  const value = selectedTags.value;
+  // Get the current URL
+  const url = new URL(window.location.href);
+
+
+  if (selectedTags.value.length > 0) {
+    // Add the query parameter to the URL
+    url.searchParams.set(key, value);
+    // Replace the current URL with the updated URL
+    history.replaceState(null, null, url.toString());
+  } else {
+    const searchParams = new URLSearchParams(url.search);
+
+    // Remove all search parameters
+    searchParams.delete(key);
+    const newUrl = url.origin + url.pathname + searchParams.toString();
+    // Replace the current URL with the new URL
+    history.replaceState(null, '', newUrl);
+
+  }
+}
+
+const onTagClickHandler = (tagIndex) => {
+  const tag = eventTags.value[tagIndex]
+  tag.selected = !tag.selected
+  // When the user selects the Tag it will be added to the selectedTags Array.
+  if (tag.selected) {
+    selectedTags.value.push(tag.title);
+  } else { // When the user unselects the Tag it will be removed from the selectedTags Array.
+    const tagIndex = selectedTags.value.findIndex((selectedTag) => {
+      return selectedTag === tag.title;
+    })
+    selectedTags.value.splice(tagIndex, 1);
+  }
+
+  // Add/Remove tags to URL params
+  updateURLParams();
+}
 </script>
 
 <template>
@@ -16,15 +104,18 @@ useHead({
       </div>
     </header>
     <section class="page-section">
-      <Tags />
+      <Tags :tags="eventTags" @tag-click="onTagClickHandler" />
       <!-- Render list of all articles in ./content/blog using `path` -->
       <!-- Provide only defined fields in the `:query` prop -->
-      <ContentList
-        path="/events"
-        :query="{
-          only: ['title', 'description', 'date', 'location', 'tags', '_path'],
-        }"
-      >
+      <ContentList path="/events" :query="{
+        only: ['title', 'description', 'date', 'location', 'tags', '_path'],
+        where: {
+          tags: {
+            // filtering through selectedTags
+            $contains: selectedTags,
+          },
+        },
+      }">
         <!-- Default list slot -->
         <template v-slot="{ list }">
           <ul class="event-list">
@@ -36,7 +127,7 @@ useHead({
 
         <!-- slot to display message when no content is found -->
         <template #not-found>
-          <p>No articles found.</p>
+          <p>No Events found.</p>
         </template>
       </ContentList>
     </section>
@@ -44,10 +135,10 @@ useHead({
 </template>
 
 <style scoped>
-
-.page-section{
+.page-section {
   @apply max-w-xl m-auto;
 }
+
 .event-list {
   @apply m-auto my-3 flex max-w-md flex-col gap-3;
 }
