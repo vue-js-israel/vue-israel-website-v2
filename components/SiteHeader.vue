d474062e5678c7634eb7ae4be395b82d28fe1a17
Alex <nechaev26g4@gmail.com>
2023-06-04 16:39:26 +0300

    minor change

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,35 +1,38 @@
 <!-- ./components/SiteHeader.vue -->
 
 <template>
-    <header class="site-header">
-      <div class="wrapper">
-        <NuxtLink to="/" class="no-underline flex">
-            <img class="w-8 mr-2" src="/logo/Vue.js-Israel-community-logo.svg
+  <header class="site-header">
+    <div class="wrapper">
+      <NuxtLink to="/" class="flex no-underline">
+        <img
+          class="mr-2 w-8"
+          src="/logo/Vue.js-Israel-community-logo.svg"
+          alt="Vue.js Israel Community Logo"
+        />
+        Vue.js Israel Community
+      </NuxtLink>
 
-" alt="Vue.js Israel Community Logo"> Vue.js Israel Community
-        </NuxtLink>
+      <nav class="site-nav">
+        <ul class="links">
+          <li class="link">
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
 
-        <nav class="site-nav">
-          <ul class="links">
-            <li class="link">
-                <NuxtLink to="/events">Events</NuxtLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  </template>
-  
-  <style scoped>
-  .site-header {
-    @apply sticky top-0 w-full p-4 bg-emerald-100 bg-opacity-40 border-b-2 border-white border-opacity-30 backdrop-blur-lg z-20;
-  }
-  
-  .site-header > .wrapper {
-    @apply flex items-center justify-between max-w-6xl m-auto;
-  }
-  
-  .site-logo {
-    @apply font-black text-lg;
-  }
-  </style>
+<style scoped>
+.site-header {
+  @apply sticky top-0 z-20 w-full border-b-2 border-white border-opacity-30 bg-emerald-100 bg-opacity-40 p-4 backdrop-blur-lg;
+}
+
+.site-header > .wrapper {
+  @apply m-auto flex max-w-6xl items-center justify-between;
+}
+
+.site-logo {
+  @apply text-lg font-black;
+}
+</style>
