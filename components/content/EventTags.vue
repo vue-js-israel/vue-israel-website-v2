10cefd8d931caa88d24ad7b3c21ea861339585b5
Alex <nechaev26g4@gmail.com>
2023-06-07 08:39:10 +0300

    minor changes in EventTags

--- components/content/EventTags.vue
+++ components/content/EventTags.vue
@@ -1,21 +1,12 @@
 <template>
-  <div class="tag-container">
-    <span class="tag" v-for="(tag, n) in $slots.default()[0].props.id.split('-')" :key="`tag-key-${tag}-${n}`">
+  <div class="flex gap-1 my-2">
+    <span class="rounded-md bg-gray-200 p-1 text-xs" v-for="(tag, n) in $slots.default()[0].props.id.split('-')" :key="`tag-key-${tag}-${n}`">
       {{ tag }}
     </span>
-  
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
 
 <style scoped>
-.tag-container {
-  @apply flex gap-1 my-2;
-}
-
-.tag-container .tag {
-  @apply rounded-md bg-gray-200 p-1 text-xs;
-}
 </style>
