b3233bfe247c095bad8c43f3db30975b6c3515e4
Alex <alex26g4@gmail.com>
2023-06-14 21:07:59 +0300

    job added job title

--- pages/speakers/[...speaker].vue
+++ pages/speakers/[...speaker].vue
@@ -1,18 +1,14 @@
 <template>
   <main class="m-auto flex max-w-6xl flex-col px-4 py-4 md:px-0">
     <div class="flex flex-col items-center justify-center">
-      <img
-        :src="speaker.image"
-        :alt="`${speaker.name}-profile-image`"
-        class="w-72 rounded-full"
-      />
-      <h1 class="m-7 text-4xl font-bold">{{ speaker.name }}</h1>
+      <img :src="speaker.image" :alt="`${speaker.name}-profile-image`" class="w-72 rounded-full" />
+      <div class="m-4 text-center">
+        <h1 class="text-4xl font-bold">{{ speaker.name }}</h1>
+        <h1 class="text-2xl">{{ speaker.title }}</h1>
+      </div>
       <div class="flex items-center gap-2">
         <p class="flex items-center">
-          <Icon name="mdi:briefcase-outline" size="35" /><span
-            class="text-2xl"
-            >{{ speaker.company }}</span
-          >
+          <Icon name="mdi:briefcase-outline" size="35" /><span class="text-2xl">{{ speaker.company }}</span>
         </p>
         <div v-for="socialLink in speaker.socialLinks" :key="socialLink">
           <a :href="socialLink" class="flex items-center">
@@ -21,25 +17,17 @@
         </div>
       </div>
     </div>
-    <div
-      v-for="{
-        link,
-        title,
-        videos,
-        slides,
-        talkTitle,
-        talkDescription,
-        poster,
-      } in speaker.events"
-      :key="title"
-      class="mt-8"
-    >
+    <div v-for="{
+      link,
+      title,
+      videos,
+      slides,
+      talkTitle,
+      talkDescription,
+      poster,
+    } in speaker.events" :key="title" class="mt-8">
       <div class="w-md grid p-2">
-        <img
-          :src="poster"
-          :alt="`${speaker.name} Event Poster`"
-          class="md:h-96 h-64 justify-self-center"
-        />
+        <img :src="poster" :alt="`${speaker.name} Event Poster`" class="md:h-96 md:w-auto w-screen justify-self-center" />
       </div>
       <div class="grid max-w-md md:grid-cols-[1fr_2fr] grid-cols-1 p-2">
         <h3 class="text-xl font-bold">Event:</h3>
@@ -56,16 +44,10 @@
       <div class="grid md:grid-cols-[1fr_2fr] grid-cols-1 p-2">
         <h3 v-if="videos" class="text-xl font-bold">Event Video:</h3>
         <div class="grid md:grid-cols-3 grid-cols-1 gap-2">
-          <iframe
-            v-for="video in videos"
-            :key="video"
-            :src="video"
-            class="w-full"
-            title="YouTube video player"
+          <iframe v-for="video in videos" :key="video" :src="video" class="w-full" title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
+            allowfullscreen></iframe>
         </div>
       </div>
       <hr />
