62638f3642e7272a3dce06757c614cfec7c56431
Alex <alex26g4@gmail.com>
2023-06-17 12:54:14 +0300

    improvement of url to mdi converter

--- pages/speakers/[...speaker].vue
+++ pages/speakers/[...speaker].vue
@@ -1,21 +1,14 @@
 <template>
   <main class="m-auto flex max-w-6xl flex-col px-4 py-4 md:px-0">
     <div class="flex flex-col items-center justify-center">
-      <img
-        :src="speaker.image"
-        :alt="`${speaker.name}-profile-image`"
-        class="w-72 rounded-full"
-      />
+      <img :src="speaker.image" :alt="`${speaker.name}-profile-image`" class="w-72 rounded-full" />
       <div class="my-4 text-center">
         <h1 class="text-4xl font-bold">{{ speaker.name }}</h1>
-      <h1 class="text-xl">{{ speaker.title }}</h1>
+        <h1 class="text-xl">{{ speaker.title }}</h1>
       </div>
       <div class="flex items-center gap-2">
         <p class="flex items-center">
-          <Icon name="mdi:briefcase-outline" size="35" /><span
-            class="text-xl"
-            >{{ speaker.company }}</span
-          >
+          <Icon name="mdi:briefcase-outline" size="35" /><span class="text-xl">{{ speaker.company }}</span>
         </p>
         <div v-for="socialLink in speaker.socialLinks" :key="socialLink">
           <a :href="socialLink" class="flex items-center">
@@ -76,13 +69,41 @@ const speaker = {
 };
 
 useHead({
-  title: `${speaker.name} | Vue.js Israel`,
-  meta: [{ name: "description", content: "Vus.js Israel's speakers" }],
+  title: `Vue.js Israel | ${speaker.name}`,
+  meta: [{ name: "description", content: "Vus.js Israel community speaker" }],
 });
 
-const socialIcon = (socialLink) => {
-  const socialName = socialLink.split(".")[1];
-  return `mdi:${socialName}`;
+const socialIcon = (url) => {
+  // Define the mapping of social media domains to MDI icon classes
+  const socialMediaMapping = {
+    'facebook.com': 'mdi:facebook',
+    'twitter.com': 'mdi:twitter',
+    'instagram.com': 'mdi:instagram',
+    'linkedin.com': 'mdi:linkedin',
+    'youtube.com': 'mdi:youtube',
+    'github.com': 'mdi:github',
+    // Add more social media domains and corresponding MDI icon classes as needed
+  };
+
+  // Remove the protocol part (http:// or https://)
+  let cleanedUrl = url.replace(/^https?:\/\//, '');
+
+  // Find the index of the first slash after the protocol
+  let slashIndex = cleanedUrl.indexOf('/');
+
+  // Extract the subdomain and domain name
+  let subdomainAndDomain = cleanedUrl.substring(0, slashIndex !== -1 ? slashIndex : cleanedUrl.length);
+
+  // Split the subdomain and domain name by dots
+  let parts = subdomainAndDomain.split('.');
+
+  // Extract the domain name
+  let domainName = parts.slice(-2).join('.');
+
+  // Look up the MDI icon class for the domain in the mapping
+  let mdiIconClass = socialMediaMapping[domainName];
+
+  return mdiIconClass || 'mdi:web'; // Return the MDI icon class or an empty string if not found
 };
 </script>
 
