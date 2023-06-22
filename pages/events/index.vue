a0c8b697f56bddd68a77128f3062afcb6e0a6d9e
Alex <nechaev26g4@gmail.com>
2023-06-22 15:43:42 +0300

    converting md to json

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,82 +1,113 @@
 <script setup>
-import { addURLParams, removeURLParams } from '@/components/utils/urlUtils';
+// import { addURLParams, removeURLParams } from "@/components/utils/urlUtils";
+import { events } from "@/content/event.json";
 
 useHead({
   title: "Vue.js Israel Events",
-  meta: [
-    { name: "description", content: "Vus.js Israel's events" },
-  ],
+  meta: [{ name: "description", content: "Vus.js Israel's events" }],
 });
 
+const eventCardData = computed(() => {
+  return events.map((event) => {
+    return {
+      eventId: event.eventId,
+      eventDate: event.eventDate,
+      eventLocationName: event.eventLocationName,
+      eventTags: event.eventTags,
+      eventTitle: event.eventTitle,
+    };
+  });
+});
+
+// const { data: data1 } = await useAsyncData(() =>
+//   queryContent("/").where({ _path: "/event", 'events.eventTags':{$ne:[]} }).only(["events"]).findOne()
+// );
+// const { data } = await useAsyncData(() => queryContent("/").where({ _path: "/event"}).only(["events"]).find());
+
+// const events = computed(() => {
+//   return data1.value.events;
+// });
+
+// const eventsTags = computed(() => {
+//   const eventsTags = events.value.map((event) => {
+//     return { eventTags: [...event.eventTags] };
+//   });
+//   return eventsTags;
+// });
+
 // get only tags data from `/events`
-const { data } = await useAsyncData("eventTags", () => queryContent("events").only(["eventTags"]).find());
+// const { data } = await useAsyncData("eventTags", () =>
+//   queryContent("events").only(["eventTags"]).find()
+// );
 
 // helper function to flatten tags array
-const flatten = (tags, key) => {
-  let _tags = tags
-    .map((tag) => {
-      let _tag = tag;
-      if (tag[key]) {
-        let flattened = flatten(tag[key]);
-        _tag = flattened;
-      }
-      return _tag;
-    })
-    .flat(1);
-  return _tags;
-};
+// const flatten = (tags, key) => {
+//   let _tags = tags
+//     .map((tag) => {
+//       let _tag = tag;
+//       if (tag[key]) {
+//         let flattened = flatten(tag[key]);
+//         _tag = flattened;
+//       }
+//       return _tag;
+//     })
+//     .flat(1);
+//   return _tags;
+// };
 
-const selectedTagsFromURL = () => {
-  const { fullPath } = useRoute();
-  const tagsString = (fullPath.split('='))[1];
-  return tagsString?.split(',') ?? [];
-}
+// const selectedTagsFromURL = () => {
+//   const { fullPath } = useRoute();
+//   const tagsString = fullPath.split("=")[1];
+//   return tagsString?.split(",") ?? [];
+// };
 
-const selectedTags = ref(selectedTagsFromURL());
-/**
- * generates an array without duplicates from flattened array
- * and initiate each instance with the title and selected property.
- */
-const interactiveTags = () => {
-  const tags = [...new Set(flatten(data.value, "eventTags"))];
-  const interactiveTags = tags.map((tag) => {
+// const selectedTags = ref(selectedTagsFromURL());
+// /**
+//  * generates an array without duplicates from flattened array
+//  * and initiate each instance with the title and selected property.
+//  */
+// const interactiveTags = () => {
+//   const tags = [...new Set(flatten(eventsTags.value, "eventTags"))];
+//   const interactiveTags = tags.map((tag) => {
+//     const selected = selectedTags.value.includes(tag);
 
-    const selected = selectedTags.value.includes(tag);
+//     return {
+//       title: tag,
+//       selected,
+//     };
+//   });
+//   return interactiveTags;
+// };
+// const eventTags = ref(interactiveTags());
 
-    return {
-      title: tag,
-      selected,
-    }
-  })
-  return interactiveTags;
-};
-const eventTags = ref(interactiveTags())
+// const onTagClickHandler = (tagIndex) => {
+//   const tag = eventTags.value[tagIndex];
+//   tag.selected = !tag.selected;
+//   // When the user selects the Tag it will be added to the selectedTags Array.
+//   if (tag.selected) {
+//     selectedTags.value.push(tag.title);
+//   } else {
+//     // When the user unselects the Tag it will be removed from the selectedTags Array.
+//     const tagIndex = selectedTags.value.findIndex((selectedTag) => {
+//       return selectedTag === tag.title;
+//     });
+//     selectedTags.value.splice(tagIndex, 1);
+//   }
 
-const onTagClickHandler = (tagIndex) => {
-  const tag = eventTags.value[tagIndex]
-  tag.selected = !tag.selected
-  // When the user selects the Tag it will be added to the selectedTags Array.
-  if (tag.selected) {
-    selectedTags.value.push(tag.title);
-  } else { // When the user unselects the Tag it will be removed from the selectedTags Array.
-    const tagIndex = selectedTags.value.findIndex((selectedTag) => {
-      return selectedTag === tag.title;
-    })
-    selectedTags.value.splice(tagIndex, 1);
-  }
+//   // Add/Remove tags to/from URL params
+//   if (selectedTags.value.length > 0) {
+//     addURLParams("tags", selectedTags.value);
+//   } else {
+//     removeURLParams("tags");
+//   }
+// };
 
-  // Add/Remove tags to/from URL params
-  if (selectedTags.value.length > 0) {
-    addURLParams('tags', selectedTags.value)
-  } else {
-    removeURLParams('tags')
-  }
-}
-
-const onTagEventClickHandler = (eventTag) => {
-  const tagIndex = eventTags.value.findIndex((mainTag) => eventTag === mainTag.title)
-  onTagClickHandler(tagIndex)
-}
+// const onTagEventClickHandler = (eventTag) => {
+//   const tagIndex = eventTags.value.findIndex(
+//     (mainTag) => eventTag === mainTag.title
+//   );
+//   onTagClickHandler(tagIndex);
+// };
 </script>
 
 <template>
@@ -87,33 +118,14 @@ const onTagEventClickHandler = (eventTag) => {
         <p class="text-lg font-medium">Events of Vus.JS Israel</p>
       </div>
     </header>
-    <section class="max-w-xl m-auto py-2">
-      <Tags :tags="eventTags" @tag-click="onTagClickHandler" />
-      <!-- Render list of all events in ./events using `path` -->
-      <!-- Provide only defined fields in the `:query` prop -->
-      <ContentList path="/events" :query="{
-        only: ['title', 'description', 'date', 'location', 'eventTags', '_path'],
-        where: {
-          eventTags: {
-            // filtering through selectedTags
-            $contains: selectedTags,
-          },
-        },
-      }">
-        <!-- Default list slot -->
-        <template v-slot="{ list }">
-          <ul class="m-auto my-3 flex max-w-md flex-col gap-3">
-            <li v-for="event in list" :key="event._path" class="event">
-              <EventsEventCard :event="event" :selectedTags="selectedTags" @tag-click="onTagEventClickHandler"/>
-            </li>
-          </ul>
-        </template>
-
-        <!-- slot to display message when no content is found -->
-        <template #not-found>
-          <p>No Events found.</p>
-        </template>
-      </ContentList>
+    <section class="m-auto max-w-xl py-2">
+      <div v-for="event in eventCardData" :key="event.eventId">
+        <EventsEventCard
+          :event="event"
+          :selectedTags="[]"
+          @tag-click="onTagEventClickHandler"/>
+      </div>
+      <!-- <Tags :tags="eventTags" @tag-click="onTagClickHandler" /> -->
     </section>
   </main>
 </template>
