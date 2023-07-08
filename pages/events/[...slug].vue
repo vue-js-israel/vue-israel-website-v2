e972e0a14eade52205f49412d449be9a00db62ea
Alex <alex26g4@gmail.com>
2023-07-08 22:30:23 +0300

    template change

--- pages/events/[...slug].vue
+++ pages/events/[...slug].vue
@@ -51,15 +51,15 @@ const singleSpeakerTitle = (speakerId) => {
 
 // returns a costume sting that chains the speaker names first then the talk title and then the speaker companies
 const multipleSpeakerTitle = (speakerIds) => {
- const speakerNames = speakerIds.map((id)=> speakers[id].name);
- const speakerCompanies = speakerIds.map((id)=> speakers[id].company);
- const uniqueCompanies = [...new Set(speakerCompanies)]
+  const speakerNames = speakerIds.map((id) => speakers[id].name);
+  const speakerCompanies = speakerIds.map((id) => speakers[id].company);
+  const uniqueCompanies = [...new Set(speakerCompanies)]
 
- const speakerNamesString = speakerNames.join(' & ');
- const speakersTalk = speakerTalk(speakerIds[0]).talkTitle
- const speakerCompaniesString = uniqueCompanies.join(' / ');
+  const speakerNamesString = speakerNames.join(' & ');
+  const speakersTalk = speakerTalk(speakerIds[0]).talkTitle
+  const speakerCompaniesString = uniqueCompanies.join(' / ');
 
- return `${speakerNamesString} - ${speakersTalk} - ${speakerCompaniesString}`;
+  return `${speakerNamesString} - ${speakersTalk} - ${speakerCompaniesString}`;
 }
 
 // set the meta
@@ -108,21 +108,22 @@ useHead({
             </div>
           </div>
 
-          <p v-else-if="sectionId === 'agenda'" v-for="{ timeSlot, title, speakerIds } in sectionContent.value"
-            :key="title">
-            <strong>{{ `${timeSlot ?? ""} ` }}</strong>
-            <span v-if="speakerIds">
-              <span v-if="speakerIds.length === 1">
-                {{ singleSpeakerTitle(speakerIds[0]) }}
+          <template v-else-if="sectionId === 'agenda'">
+            <p v-for="{ timeSlot, title, speakerIds } in sectionContent.value" :key="title">
+              <strong>{{ `${timeSlot ?? ""} ` }}</strong>
+              <span v-if="speakerIds">
+                <span v-if="speakerIds.length === 1">
+                  {{ singleSpeakerTitle(speakerIds[0]) }}
+                </span>
+                <span v-else>
+                  {{ multipleSpeakerTitle(speakerIds) }}
+                </span>
               </span>
               <span v-else>
-                {{ multipleSpeakerTitle(speakerIds) }}
+                {{ title }}
               </span>
-            </span>
-            <span v-else>
-              {{ title }}
-            </span>
-          </p>
+            </p>
+          </template>
 
           <hr class="mt-2 mb-12" />
         </div>
