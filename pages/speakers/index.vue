8f7d7b3ce653fc299749a3242e685658df6b97c8
Alex <alex26g4@gmail.com>
2023-07-17 23:13:35 +0300

    Restructure of speaker page

--- pages/speakers/index.vue
+++ pages/speakers/index.vue
@@ -3,9 +3,12 @@
 		<div class="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10">
 			<p class="p-2 text-sm font-medium tracki text-center uppercase">Speakers</p>
 			<h1 class="text-4xl font-bold leadi text-center sm:text-5xl">Vue.js Israel community speakers</h1>
-			<div class="flex flex-row flex-wrap-reverse justify-center mt-8">
-				<SpeakersSpeakerCard v-for="(speaker, index) in speakersList" :key="`${speaker.name}-${index}-key`"
-					:speaker="speaker" />
+			<div class="flex flex-row flex-wrap-reverse justify-center gap-4 mt-8">
+				<template v-for="talk in talks" :key="`${talk.talkId}-key`">
+					<template v-for="speakerId in talk.speakerIds" :key="`${speakerId}-key`">
+						<SpeakersSpeakerCard :speaker="speakers[speakerId]" :talk="talk"/>
+					</template>
+				</template>
 			</div>
 		</div>
 	</section>
@@ -13,19 +16,12 @@
 
 <script setup>
 import { speakers } from '@/content/speakers.json'
-
+import { talks } from '@/content/talks.json'
 useHead({
 	title: "Vue.js Israel Speakers",
 	meta: [{ name: "description", content: "Vus.js Israel's speakers" }],
 });
 
-const speakersList = computed(() => {
-	const speakersList = Object.entries(speakers).map(([id, speaker]) => {
-		return { id, ...speaker };
-	});
-	
-	return speakersList
-})
 </script>
 
 <style scoped></style>
