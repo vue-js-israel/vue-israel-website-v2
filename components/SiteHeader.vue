46dd823fb3b0f02b6873620b834012934e6a1120
Alex <alex26g4@gmail.com>
2023-07-19 21:41:40 +0300

    applied color schema and added nav for mobile version

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,69 +1,77 @@
 <!-- ./components/SiteHeader.vue -->
 
 <template>
-  <header
-    class="max-h-24 min-h-fit p-4 bg-light-second dark:bg-dark-second text-dark-second dark:text-light-second"
-  >
+  <header class="max-h-24 min-h-fit p-4 border-light-bg-lightest dark:border-dark-bg-dark border-b-2">
+    <div v-if="menuToggle"
+      class="md:hidden absolute top-0 left-0 right-0 bottom-0 z-50 w-full opacity-95 dark:bg-dark-bg-darker bg-white py-7 px-4 flex justify-between">
+      <ul class="flex flex-col gap-10 justify-center text-3xl">
+        <li>
+          <NuxtLink @click="onMenuClose" class="flex items-center border-b-2 px-4 dark:border-transparent link"
+            to="/events">Events
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink @click="onMenuClose" class="flex items-center border-b-2 px-4 dark:border-transparent link"
+            to="/speakers">Speakers
+          </NuxtLink>
+        </li>
+      </ul>
+      <button @click.prevent="onMenuClose" class="flex justify-end p-4 md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-14 w-14">
+          <path fill="currentColor"
+            d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6L6.4 19Z" />
+        </svg>
+      </button>
+    </div>
     <div class="container mx-auto flex h-16 justify-between">
-      <NuxtLink
-        to="/"
-        aria-label="Back to homepage"
-        class="flex items-center p-2"
-      >
-        <img
-          class="mr-2 w-8"
-          src="/logo/Vue.js-Israel-community-logo.svg"
-          alt="Vue.js Israel Community Logo"
-        />
+      <NuxtLink to="/" aria-label="Back to homepage" class="flex items-center p-2">
+        <img class="mr-2 w-8" src="/logo/Vue.js-Israel-community-logo.svg" alt="Vue.js Israel Community Logo" />
         Vue.js Israel Community
       </NuxtLink>
       <ul class="hidden items-stretch space-x-3 md:flex">
         <li class="flex">
-          <NuxtLink
-            class="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent"
-            to="/events"
-            >Events</NuxtLink
-          >
+          <NuxtLink class="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent link" to="/events">Events
+          </NuxtLink>
         </li>
         <li class="flex">
-          <NuxtLink
-            class="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent"
-            to="/speakers"
-            >Speakers
+          <NuxtLink class="-mb-1 flex items-center border-b-2 px-4 dark:border-transparent link" to="/speakers">Speakers
           </NuxtLink>
         </li>
       </ul>
-      <button class="flex justify-end p-4 md:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          class="h-6 w-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
+      <button @click.prevent="onMenuOpen" class="flex justify-end p-4 md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
         </svg>
       </button>
     </div>
   </header>
 </template>
 
+<script setup>
+
+const menuToggle = ref(false)
+
+const onMenuOpen = () => {
+  menuToggle.value = true
+  document.body.style.overflow = 'hidden';
+}
+const onMenuClose = () => {
+  menuToggle.value = false
+  document.body.style.overflow = 'auto';
+}
+</script>
+
 <style scoped>
-a.router-link-active {
+a.router-link-active.link {
   font-weight: bold;
 }
 
 /* exact link will show the primary color for only the exact matching link */
-a.router-link-exact-active {
-  @apply border-violet-400 text-violet-400;
+a.router-link-exact-active.link {
+  @apply border-cta-base text-cta-base;
 }
 
 a:hover {
-  @apply text-violet-400;
+  @apply text-cta-hover;
 }
 </style>
