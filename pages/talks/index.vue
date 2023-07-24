be1607b5c7cba3bbaec9aceec21a2abc95d8e32e
Alex Nechayev <alex.nechayev.1@gmail.com>
2023-07-24 19:16:07 +0300

    talks page init

---
+++ pages/talks/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container md:max-w-6xl mx-auto flex flex-col items-center justify-center p-4 py-6 sm:p-10">
+    <a
+      class="group mx-auto block max-w-sm gap-3 hover:no-underline focus:no-underline dark:bg-gray-900 sm:max-w-full lg:grid lg:grid-cols-12"
+    >
+      <img
+        src="https://source.unsplash.com/random/480x360"
+        alt=""
+        class="w-full rounded object-cover dark:bg-gray-500 lg:col-span-3"
+      />
+      <div class="space-y-2 p-6 lg:col-span-9">
+        <h3
+          class="text-2xl font-semibold group-hover:underline group-focus:underline sm:text-4xl"
+        >
+          Noster tincidunt reprimique ad pro
+        </h3>
+        <span class="text-xs dark:text-gray-400">February 19, 2021</span>
+        <p>
+          Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in
+          graece fuisset, eos affert putent doctus id.
+        </p>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
