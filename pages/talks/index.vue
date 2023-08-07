14a8d53d7114df686550874bc9e69467d293c630
Alex Nechayev <alex.nechayev.1@gmail.com>
2023-08-07 21:33:20 +0300

    filtered talks by url param

--- pages/talks/index.vue
+++ pages/talks/index.vue
@@ -4,9 +4,9 @@
       Talks
     </p>
     <div class="mt-8 flex flex-row flex-wrap-reverse justify-center gap-4">
-      <template v-for="talk in talks" :key="talk.talkId">
+      <template v-for="talk in filteredTalks" :key="talk.talkId">
         <template v-for="speakerId in talk.speakerIds" :key="speakerId">
-          <TalksTalkCard :speaker="speakers[speakerId]" :talk="talk" />
+          <TalksTalkCard v-if="speakerId === sid" :speaker="speakers[speakerId]" :talk="talk" />
         </template>
       </template>
     </div>
@@ -20,6 +20,16 @@ useHead({
   title: "Vue.js Israel Speakers",
   meta: [{ name: "description", content: "Vus.js Israel's speakers" }],
 });
+
+const { query } = useRoute();
+const { sid } = query;
+
+const filteredTalks = computed(() => {
+  const filteredTalks = talks.filter((talk) => {
+    return talk.speakerIds.includes(sid);
+  })
+  return filteredTalks.length > 0 ? filteredTalks : talks;
+})
 </script>
 
 <style scoped></style>
