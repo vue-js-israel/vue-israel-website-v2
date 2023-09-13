711a1d8c616417670a4355b075c67943875425e8
Alex Nechayev <alex.nechayev.1@gmail.com>
2023-09-13 16:40:12 +0300

    fixed tags sorting bug

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -6,17 +6,23 @@ useHead({
   meta: [{ name: "description", content: "Vus.js Israel's events" }],
 });
 
-const eventList = Object.entries(events).map(([eventId, event]) => { return { ...event, eventId } })
+const eventList = Object.entries(events).map(([eventId, event]) => {
+  return { ...event, eventId };
+});
 
 const tagsObject = computed(() => {
-  const eventTags = eventList.map((event) => event.eventTags)
-  const uniqueEventTags = [...new Set(...eventTags)];
+  const eventTags = eventList.map((event) => event.eventTags);
+  const flattenEventTags = eventTags.flat();
+  const uniqueEventTags = [...new Set(flattenEventTags)];
+  const tagsInAlphabeticOrder = uniqueEventTags.sort((tagA, tagB) =>
+    tagA.localeCompare(tagB)
+  );
   // converts an array to an object
-  return uniqueEventTags.reduce((obj, item,) => {
+  return uniqueEventTags.reduce((obj, item) => {
     obj[item] = item;
     return obj;
   }, {});
-})
+});
 
 const selectedTags = computed(() => {
   const { query } = useRoute();
@@ -28,24 +34,30 @@ const filteredEvents = computed(() => {
   if (selectedTags.value.length > 0) {
     return eventList.filter((event) => {
       return event.eventTags.find((eventTag) => {
-        return selectedTags.value.includes(eventTag)
-      })
-    })
+        return selectedTags.value.includes(eventTag);
+      });
+    });
   } else {
-    return eventList
+    return eventList;
   }
-})
+});
 </script>
 
 <template>
-  <div class="container mx-auto space-y-16 ">
-    <div class="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10">
+  <div class="container mx-auto space-y-16">
+    <div
+      class="container flex flex-col items-center justify-center p-4 mx-auto sm:p-10"
+    >
       <p class="p-2 text-md font-medium tracki text-center uppercase">Events</p>
       <div class="w-full">
         <TagsController :tags="tagsObject" />
       </div>
       <section class="my-5">
-        <EventsCard v-for="event in filteredEvents" :key="event.eventId" :event="event" />
+        <EventsCard
+          v-for="event in filteredEvents"
+          :key="event.eventId"
+          :event="event"
+        />
       </section>
     </div>
   </div>
