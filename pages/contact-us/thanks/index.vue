35fe601a560293f9b82af23bc2ff10d8972165e2
Alex Nechayev <alex.nechayev.1@gmail.com>
2023-10-23 19:46:24 +0300

    initialization of contact us page

---
+++ pages/contact-us/thanks/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container mx-auto flex justify-center flex-col">
+        <div class="bg-dark-bg-darker max-w-md mx-auto p-7 rounded-lg">
+            <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+                    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+                        <path d="M44 24V9H4v30h20m7-3l5 4l8-10" />
+                        <path d="m4 9l20 15L44 9" />
+                    </g>
+                </svg>
+                <h1 class="text-4xl">Thanks!</h1>
+            </div>
+            <div class="text-2xl">
+                <p>The form was submitted successfully.</p>
+                <NuxtLink class="text-cta-base hover:text-cta-hover" to="/">Back to homepage</NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
