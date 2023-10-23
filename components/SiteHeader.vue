a8fb7fc132322b5e32c2c3c0fe0185ccdda30ec0
Alex Nechayev <alex.nechayev.1@gmail.com>
2023-10-23 19:48:33 +0300

    Contact us added to site's navbar

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,7 +1,20 @@
-<!-- ./components/SiteHeader.vue -->
+<script setup>
+
+const menuToggle = ref(false)
+
+const openMenu = () => {
+  menuToggle.value = true
+  document.body.style.overflow = 'hidden';
+}
+const closeMenu = () => {
+  menuToggle.value = false
+  document.body.style.overflow = 'auto';
+}
+</script>
 
 <template>
-  <header class="max-h-24 min-h-fit p-4 bg-dark-bg-dark text-dark-text-lightest border-light-bg-lighter dark:border-dark-bg-dark border-b-2">
+  <header
+    class="max-h-24 min-h-fit p-4 bg-dark-bg-dark text-dark-text-lightest border-light-bg-lighter dark:border-dark-bg-dark border-b-2">
     <div v-if="menuToggle"
       class="md:hidden absolute top-0 left-0 right-0 bottom-0 z-50 w-full opacity-95 dark:bg-dark-bg-darker bg-white py-7 px-4 flex justify-between">
       <ul class="flex flex-col gap-10 justify-center text-3xl">
@@ -16,8 +29,13 @@
           </NuxtLink>
         </li>
         <li>
+          <NuxtLink @click="closeMenu" class="flex items-center border-b-2 px-4 dark:border-transparent link" to="/talks">
+            Talks
+          </NuxtLink>
+        </li>
+        <li>
           <NuxtLink @click="closeMenu" class="flex items-center border-b-2 px-4 dark:border-transparent link"
-            to="/talks">Talks
+            to="/contact-us">Contact Us
           </NuxtLink>
         </li>
       </ul>
@@ -27,7 +45,8 @@
     </div>
     <div class="container mx-auto flex h-16 justify-between">
       <NuxtLink to="/" aria-label="Back to homepage" class="flex items-center p-2">
-        <img class="mr-2 w-8" src="/logo/Vue.js-Israel-community-logo.svg" alt="Vue.js Israel Community Logo" />
+        <NuxtImg src="/logo/Vue.js-Israel-community-logo.svg" alt="Vue.js Israel Community Logo" width="32px"
+          height="28px" />
         Vue.js Israel Community
       </NuxtLink>
       <ul class="hidden items-stretch space-x-3 md:flex">
@@ -43,27 +62,17 @@
           <NuxtLink class="-mb-1 flex items-center border-b-2 px-4 border-transparent link" to="/talks">Talks
           </NuxtLink>
         </li>
+        <li class="flex">
+          <NuxtLink class="-mb-1 flex items-center border-b-2 px-4 border-transparent link" to="/contact-us">Contact Us
+          </NuxtLink>
+        </li>
       </ul>
-      <button @click.prevent="openMenu" class="flex justify-end p-4 md:hidden">
+      <button @click.prevent="openMenu" aria-label="menu button" class="flex justify-end p-4 md:hidden">
         <Icon name="mdi:menu" size="30" />
       </button>
     </div>
   </header>
 </template>
-
-<script setup>
-
-const menuToggle = ref(false)
-
-const openMenu = () => {
-  menuToggle.value = true
-  document.body.style.overflow = 'hidden';
-}
-const closeMenu = () => {
-  menuToggle.value = false
-  document.body.style.overflow = 'auto';
-}
-</script>
 
 <style scoped>
 a.router-link-active.link {
