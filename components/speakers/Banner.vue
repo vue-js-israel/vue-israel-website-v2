f3c81c27e00119297b9b2df1e27620a28b3bcbdc
Alex Nechayev <alex.nechayev.1@gmail.com>
2023-11-05 18:08:58 +0200

    init of speakers banner

---
+++ components/speakers/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-cta-base p-6 py-12 dark:text-gray-900">
+    <div class="container mx-auto">
+      <div class="flex flex-col items-center justify-between lg:flex-row">
+        <h2 class="tracki text-center text-6xl font-bold">
+          Calling all <br class="sm:hidden" />speakers!
+        </h2>
+        <div class="space-x-2 py-2 text-center lg:py-0">
+          <span>Share your insights at our</span>
+          <span class="text-lg font-bold">next event</span>
+        </div>
+        <NuxtLink
+          href="#"
+          target="blank"
+          class="mt-4 block rounded-md border border-gray-400 bg-gray-50 px-5 py-3 text-gray-900 lg:mt-0"
+          >Apply Now</NuxtLink
+        >
+      </div>
+    </div>
+  </div>
+</template>
