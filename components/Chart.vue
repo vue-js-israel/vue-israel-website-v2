221e506ac763d7b6a08b92c7575284576beede69
Yarin Mizrachi <yarin@beprofit.co>
2024-03-08 15:29:26 +0200

    added apexcharts to project + created statistics page

---
+++ components/Chart.vue
@@ -0,0 +1,18 @@
+<template>
+  <ClientOnly>
+    <apexchart type="area" :options="chartOptions" :series="series" />
+  </ClientOnly>
+</template>
+
+<script setup>
+const props = defineProps({
+  chartOptions: {
+    type: Object,
+    default: () => ({}),
+  },
+  series: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
---
+++ pages/statistics/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import platforms from "@/content/platforms.json";
+import Card from "@/components/statistics/Card.vue";
+import platformsStatistics from "@/content/statistics.json";
+
+const statistics = platforms.map((platform) => ({
+  ...platform,
+  ...platformsStatistics[platform.name],
+}));
+</script>
+
+<template>
+  <div
+    class="container mx-auto flex flex-col items-center justify-center p-4 sm:p-10">
+    <p class="text-md p-2 text-center font-medium uppercase">Statistics</p>
+    <section class="my-6 p-6">
+      <div
+        class="container mx-auto grid grid-cols-1 gap-10 sm:grid-cols-2 xl:grid-cols-3">
+        <Card v-for="stat in statistics" :key="stat.title" :stat="stat" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
