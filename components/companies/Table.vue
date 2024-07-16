e2aa2704f1466480254a2c4741445b168945e551
Alex Nechayev <64922097+AlexNechayev@users.noreply.github.com>
2024-07-16 14:12:56 +0000

    improve the table structure in mobile view

--- components/companies/Table.vue
+++ components/companies/Table.vue
@@ -2,82 +2,46 @@
   <div
     class="container mx-auto flex flex-col items-center justify-center rounded-lg p-4 py-6 sm:p-10 md:max-w-6xl">
     <table
-      class="w-full whitespace-nowrap p-6 text-left text-xs text-white sm:text-sm md:text-base">
-      <colgroup>
-        <col v-for="(column, index) in columns" :key="index" />
-      </colgroup>
+      class="w-full max-w-3xl whitespace-nowrap p-6 text-left text-xs text-white sm:text-sm md:text-base">
       <thead>
         <tr class="bg-gray-800 font-normal">
-          <th v-for="(column, index) in columns" :key="index" class="p-3">
-            {{ column.label }}
-          </th>
+          <th class="w-32 px-3 py-2 md:w-72">Company Name</th>
+          <th class="px-3 py-2">City</th>
+          <th class="px-3 py-2">Links</th>
         </tr>
       </thead>
       <tbody class="border-b border-gray-600 bg-gray-700 font-light">
-        <template v-for="(item, itemIndex) in props.data" :key="itemIndex">
-          <tr>
-            <td
-              v-for="(column, colIndex) in columns"
-              :key="colIndex"
-              class="px-3 py-2">
-              <template v-if="column.key === 'link'">
-                <div class="mt-4 flex items-center space-x-4">
-                  <template v-if="item.linkedin">
-                    <a :href="item.linkedin">
-                      <Icon
-                        name="mdi:linkedin"
-                        width="2rem"
-                        height="2rem"
-                        style="color: white"
-                        class="h-8 w-8 text-blue-500 dark:text-blue-300" />
-                    </a>
-                  </template>
-                  <template v-if="item.website">
-                    <a :href="item.website">
-                      <Icon
-                        name="mdi:web"
-                        width="2em"
-                        height="2rem"
-                        style="color: white" />
-                    </a>
-                  </template>
-                  <template v-if="item.facebook">
-                    <a :href="item.facebook">
-                      <Icon
-                        name="mdi:facebook"
-                        width="2em"
-                        height="2rem"
-                        style="color: white" />
-                    </a>
-                  </template>
-                  <template v-if="item.github">
-                    <a :href="item.facebook">
-                      <Icon
-                        name="mdi:github"
-                        width="2em"
-                        height="2rem"
-                        style="color: white" />
-                    </a>
-                  </template>
-                </div>
-              </template>
-              <template v-if="column.key === 'company'">
-                <div class="flex items-center gap-2">
-                  <div
-                    class="relative flex h-8 w-8 items-center justify-center overflow-hidden border border-gray-300 bg-white">
-                    <CompaniesLogo
-                      :domain="item['logo']"
-                      :size="24"></CompaniesLogo>
-                  </div>
-                  <span class="ml-2">{{ item[column.key] }}</span>
-                </div>
-              </template>
-              <template v-else>
-                <p>{{ item[column.key] }}</p>
-              </template>
-            </td>
-          </tr>
-        </template>
+        <tr
+          v-for="{ logo, company, location, links } in companiesData"
+          :key="company">
+          <td class="flex w-32 items-center gap-2 px-3 py-2 md:w-72">
+            <div
+              class="relative flex h-8 w-8 items-center justify-center overflow-hidden border border-gray-300 bg-white">
+              <CompaniesLogo :domain="logo" :size="24"></CompaniesLogo>
+            </div>
+            <span class="ml-2 whitespace-break-spaces">{{ company }}</span>
+          </td>
+
+          <td class="px-3 py-2">
+            <p>{{ location }}</p>
+          </td>
+
+          <td
+            class="grid grid-cols-2 grid-rows-2 place-content-center px-3 py-2 md:grid-cols-4 md:grid-rows-1 md:place-content-start">
+            <NuxtLink
+              v-for="companyLink in links"
+              :key="companyLink.url"
+              :to="companyLink.url"
+              class="flex items-center justify-center md:justify-start">
+              <Icon
+                :name="`mdi:${companyLink.name}`"
+                width="2rem"
+                height="2rem"
+                style="color: white"
+                class="h-8 w-8 text-blue-500 dark:text-blue-300" />
+            </NuxtLink>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -86,26 +50,11 @@
 <script setup>
 import { defineProps } from "vue";
 
-const props = defineProps({
-  data: {
+const { companiesData } = defineProps({
+  companiesData: {
     type: Array,
     required: true,
     default: () => [],
   },
 });
-
-const columns = [
-  { key: "company", label: "Company Name" },
-  { key: "location", label: "City" },
-  { key: "link", label: "Links" },
-];
-
-const sortedData = props.data.reduce((acc, item) => {
-  const firstLetter = item.company[0].toUpperCase();
-  if (!acc[firstLetter]) {
-    acc[firstLetter] = [];
-  }
-  acc[firstLetter].push(item);
-  return acc;
-}, {});
 </script>
