714f1e9dbcc74205bd2f155234c75b5c8527b0e6
Alex Nechayev <alex.nechayev.1@gmail.com>
2024-07-17 21:16:47 +0300

    table layout change to match mobile display

--- components/companies/Table.vue
+++ components/companies/Table.vue
@@ -2,43 +2,35 @@
   <div
     class="container mx-auto flex flex-col items-center justify-center rounded-lg p-4 py-6 sm:p-10 md:max-w-6xl">
     <table
-      class="w-full max-w-3xl whitespace-nowrap p-6 text-left text-xs text-white sm:text-sm md:text-base">
+      class="w-full whitespace-nowrap p-6 text-left text-xs text-white sm:text-sm md:text-base">
       <thead>
-        <tr class="bg-gray-800 font-normal">
-          <th class="w-32 px-3 py-2 md:w-72">Company Name</th>
-          <th class="px-3 py-2">City</th>
-          <th class="px-3 py-2">Links</th>
+        <tr class="flex flex-row items-center bg-gray-800 p-2 font-normal">
+          <th class="w-1/3">Company Name</th>
+          <th class="w-1/3">City</th>
+          <th class="w-1/3">Links</th>
         </tr>
       </thead>
-      <tbody class="border-b border-gray-600 bg-gray-700 font-light">
+      <tbody class="border-b border-gray-800 bg-gray-700 font-light">
         <tr
           v-for="{ logo, company, location, links } in companiesData"
-          :key="company">
-          <td class="flex w-32 items-center gap-2 px-3 py-2 md:w-72">
-            <div
-              class="relative flex h-8 w-8 items-center justify-center overflow-hidden border border-gray-300 bg-white">
-              <CompaniesLogo :domain="logo" :size="24"></CompaniesLogo>
-            </div>
-            <span class="ml-2 whitespace-break-spaces">{{ company }}</span>
+          :key="company"
+          class="flex flex-row items-center p-2">
+          <td class="flex w-4/12 items-center gap-2">
+            <CompaniesLogo :domain="logo" :size="24"></CompaniesLogo>
+            <span class="whitespace-break-spaces">{{ company }}</span>
           </td>
 
-          <td class="px-3 py-2">
+          <td class="w-4/12">
             <p>{{ location }}</p>
           </td>
 
-          <td
-            class="grid w-20 grid-cols-2 grid-rows-2 place-content-center px-3 py-2 md:w-64 md:grid-cols-4 md:grid-rows-1 md:place-content-start">
+          <td class="grid w-4/12 grid-cols-2 gap-y-4 md:grid-cols-4">
             <NuxtLink
               v-for="companyLink in links"
               :key="companyLink.url"
               :to="companyLink.url"
-              class="flex items-center justify-center md:justify-start">
-              <Icon
-                :name="`mdi:${companyLink.name}`"
-                width="2rem"
-                height="2rem"
-                style="color: white"
-                class="h-8 w-8 text-blue-500 dark:text-blue-300" />
+              class="">
+              <Icon :name="`mdi:${companyLink.name}`" :size="40" />
             </NuxtLink>
           </td>
         </tr>
