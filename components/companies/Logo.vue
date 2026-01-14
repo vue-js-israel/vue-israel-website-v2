e04d5c6d50884af8df15b8782db6d06e761ee249
Alex Nechayev <alex.nechayev.1@gmail.com>
2026-01-14 22:11:51 +0200

    fix: Remove unnecessary defineProps imports (auto-imported in script setup)

--- components/companies/Logo.vue
+++ components/companies/Logo.vue
@@ -7,8 +7,6 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue";
-
 const props = defineProps({
   domain: { type: String, default: "logo.com" },
   size: { type: Number, default: 16 },
